6e28d5007e135359fc6d33a133d97aba2707c19a
JaretWright <jaret.wright@georgiancollege.ca>
2023-01-12 17:50:52 -0500

    backward name challenge

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        for(int i=0; i<=100; i=i+10)
+        String name = "Jaret";
+        for (int i=name.length()-1; i>=0;--i)
         {
-            //%d is a placeholder for a number in base 10 (i.e. int)
-            //%s is a placeholder for a String
-            //%n is a new line character
-            System.out.printf("%3d %s %n",i, (i%20 == 0)?"Java":"");
+            System.out.print(name.charAt(i));
         }
+
+//        for(int i=0; i<=100; i=i+10)
+//        {
+//            //%d is a placeholder for a number in base 10 (i.e. int)
+//            //%s is a placeholder for a String
+//            //%n is a new line character
+//            System.out.printf("%3d %s %n",i, (i%20 == 0)?"Java":"");
+//        }
     }
 }
