b8003f7ffe00cf449b9c168b4268222826a67381
JaretWright <jaret.wright@georgiancollege.ca>
2023-01-12 17:40:05 -0500

    Modified the print statement to use formatted print

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        for(int i=0; i<=100; i=i+10)
+        {
+            //%d is a placeholder for a number in base 10 (i.e. int)
+            //%s is a placeholder for a String
+            //%n is a new line character
+            System.out.printf("%3d %s %n",i);
+        }
     }
 }
